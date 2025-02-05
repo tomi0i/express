92d6e490ec04c947561fb0c385fbb2ed73e179e4
tomi0i <sp.tomi0130@gmail.com>
2025-02-06 06:10:17 +0900

    add cat api

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
+var notesRouter = require('./routes/notes');
+var catRouter = require('./routes/cat');
 
 var app = express();
 
@@ -23,11 +25,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
+app.use('/notes', notesRouter); 
+app.use('/cat',catRouter);
+  
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
 
 // error handler
 app.use(function(err, req, res, next) {
